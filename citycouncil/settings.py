a5ab2087068e729bb9acc8f7dbfda65b6f39670c
maneesha <ahseenam@gmail.com>
2014-12-19 13:44:34 -0500

    move db settings to local_settings

--- citycouncil/settings.py
+++ citycouncil/settings.py
@@ -43,7 +43,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    #'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
@@ -57,10 +57,6 @@ WSGI_APPLICATION = 'citycouncil.wsgi.application'
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
 }
 
 # Internationalization
@@ -83,6 +79,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 try:
-    from local_settings import *
+    from citycouncil.local_settings import *
 except ImportError:
     pass
