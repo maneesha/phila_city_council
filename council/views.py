d70c88464f2161958e4be3322e918d2bad27077b
maneesha <ahseenam@gmail.com>
2014-12-20 16:30:53 -0500

    add view with a template

--- council/views.py
+++ council/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from council.models import Councilmember
+from django.template import RequestContext, loader
 
 # Create your views here.
 
@@ -10,7 +11,13 @@ from council.models import Councilmember
 
 #Make the view actually do something - print alpha list of all councilmembers
 #BUT html is hardcoded in view - we want to separate this into a template
+# def index(request):
+#     ordered_list = Councilmember.objects.order_by('last_name')
+#     output = "<br>".join([p.last_name + " " + p.first_name for p in ordered_list])
+#     return HttpResponse(output)
+
 def index(request):
     ordered_list = Councilmember.objects.order_by('last_name')
-    output = "<br>".join([p.last_name + " " + p.first_name for p in ordered_list])
-    return HttpResponse(output)
+    template = loader.get_template('council/index.html')
+    context = RequestContext(request, {'ordered_list':ordered_list,})
+    return HttpResponse(template.render(context))
