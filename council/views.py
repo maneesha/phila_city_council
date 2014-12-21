ff781e0182cb3f2b12bf8c270547693620b7a034
maneesha <ahseenam@gmail.com>
2014-12-21 18:26:03 -0500

    update simple list page to sort by last name

--- council/views.py
+++ council/views.py
@@ -6,7 +6,10 @@ from council.models import Councilmember, Term
 from council.forms import LastNameForm
 
 def all_councilmembers(request):
-    members = Term.objects.all()
+    """
+    Simple view to show all councilmembers' info on a page
+    """
+    members = Term.objects.all().order_by('councilperson_id__last_name')
     page = "council/all_members.html"
     return render(request, page, {'members':members})
 
