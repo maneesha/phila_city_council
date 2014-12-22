26253f1d9506701bb01242e5de305e23e4660a8e
maneesha <ahseenam@gmail.com>
2014-12-22 13:26:14 -0500

    got select rows in table

---
+++ council/tables.py
@@ -0,0 +1,9 @@
+import django_tables2 as tables 
+from council.models import Councilmember, Term
+
+class MemberTable(tables.Table):
+    class Meta:
+        model = Term
+        attrs = {"class":"paleblue"}
+        fields = ('councilperson_id.first_name','councilperson_id.last_name', 'councilperson_id.birthdate','councilperson_id.race', 'councilperson_id.gender','party', 'actual_start_date', 'actual_end_date')
+
--- council/views.py
+++ council/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 
+from django_tables2   import RequestConfig
+from council.tables import MemberTable
+
 
 from council.models import Councilmember, Term
 from council.forms import LastNameForm
@@ -9,7 +12,10 @@ def all_councilmembers(request):
     """
     Simple view to show all councilmembers' info on a page
     """
-    members = Term.objects.all().order_by('councilperson_id__last_name')
+    #This is how I'd get the object if I was making the table myself
+    #members = Term.objects.all()#.order_by('councilperson_id__last_name')
+    members = MemberTable(Term.objects.all())
+    RequestConfig(request, paginate={"per_page": 100}).configure(members)
     page = "council/all_members.html"
     return render(request, page, {'members':members})
 
