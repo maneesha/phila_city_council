f76d21d99a524d397e60d2f9ab37ee90f203a36f
maneesha <ahseenam@gmail.com>
2014-12-23 11:26:28 -0500

    add district & departed to table

--- council/tables.py
+++ council/tables.py
@@ -5,5 +5,5 @@ class MemberTable(tables.Table):
     class Meta:
         model = Term
         attrs = {"class":"paleblue"}
-        fields = ('councilperson_id.first_name','councilperson_id.last_name', 'councilperson_id.birthdate','councilperson_id.race', 'councilperson_id.gender','party', 'actual_start_date', 'actual_end_date')
+        fields = ('councilperson_id.first_name','councilperson_id.last_name', 'councilperson_id.birthdate','councilperson_id.race', 'councilperson_id.gender','party', 'district', 'actual_start_date', 'actual_end_date', 'departed')
 
