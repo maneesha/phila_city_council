2e19976d6d8e492585321234113770497e4218a4
maneesha <ahseenam@gmail.com>
2014-12-24 14:55:52 -0500

    data passes correctly from django view to js in html template

--- council/views.py
+++ council/views.py
@@ -33,8 +33,21 @@ def unique_councilmembers(request):
     #select distinct councilperson_id_id, district from council_term where actual_end_date > '1979' order by district;
     members = Term.objects.filter(actual_end_date__gte='1980')
     members = members.filter(district__gte=1)
-    members = members.values(r'district').annotate(Count(r'councilperson_id_id'))
-    json_members = json.dumps(list(members))
+    members = members.values('district').annotate(Count('councilperson_id_id'))
+
+    unique_list = []
+
+    k = 0
+
+    for member in members:
+        print(member)
+        print(type(member))
+        #will need to do something like this to get councilmember names in this dataset
+        member[k+1] = k+2
+        k+=5
+        unique_list.append(member)
+
+    json_members = json.dumps(unique_list)
 
     page = "council/unique_councilmembers.html"
     return render(request, page, {'members':members,'json_members':json_members})
