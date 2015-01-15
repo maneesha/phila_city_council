2532730b7599434acf2ab9ae1d977bc8ca26d2a6
maneesha <ahseenam@gmail.com>
2015-01-15 14:41:30 -0500

    weirdly coerced demographic pie chart horiz. layout

--- council/static/demographics.js
+++ council/static/demographics.js
@@ -1,7 +1,7 @@
 //Set width, height, radius, angle for all charts
-var w = 400;
+var w = 300;
 var h = 400;
-var r = h/2;
+var r = w/2;
 
 //Gets angle of arc
 //Used to position pie slice labels below
@@ -14,11 +14,15 @@ var getAngle = function (d) {
 ///////////////////////
 
 //Give the chart a div to go into, root svg element, a data source, attr of width, attr height, g element (groups svg shapes to work with them as if a single element), translate is a transform function that moves object x, y units
-var vis = d3.select('#race-chart').append("svg:svg").data([race_list]).attr("width", w).attr("height", h).append("svg:g").attr("transform", "translate(" + r + "," + r + ") rotate(1.6)");
+var vis = d3.select('#race-chart').append("svg:svg").data([race_list]).attr("width", w).attr("height", h).append("svg:g").attr("transform", "translate(" + r + "," + (r+30) + ") rotate(1.6)");
 var pie = d3.layout.pie().value(function(d){return d.councilperson_id_id__count;});
+
+console.log("RACE w, h, r:", w, h, r)
 
 // declare an arc generator function
 var arc = d3.svg.arc().outerRadius(r);
+
+console.log("RACE arc: ", arc);
 
 // select paths, use arc generator to draw
 var arcs = vis.selectAll("g.slice").data(pie).enter().append("svg:g").attr("class", "slice");
@@ -48,7 +52,7 @@ arcs.append("svg:text")
         return race_list[i].councilperson_id_id__race + ": " + race_list[i].councilperson_id_id__count;});
 
 arcs.append("svg:text")
-.attr("transform", "translate(-200, -170)")
+.attr("transform", "translate(-20, -160)")
 .style("font-weight", "bold")
 .text("RACE");
 
@@ -57,12 +61,15 @@ arcs.append("svg:text")
 ///////////////////////
 
 //Give the chart a div to go into, root svg element, a data source, attr of width, attr height, g element (groups svg shapes to work with them as if a single element), translate is a transform function that moves object x, y units
-var vis = d3.select('#gender-chart').append("svg:svg").data([gender_list]).attr("width", w).attr("height", h).append("svg:g").attr("transform", "translate(" + r + "," + r + ") rotate(1.6)");
+var vis = d3.select('#gender-chart').append("svg:svg").data([gender_list]).attr("width", w).attr("height", h).append("svg:g").attr("transform", "translate(" + r + "," + (r+30) + ") rotate(1.6)");
 var pie = d3.layout.pie().value(function(d){return d.councilperson_id_id__count;});
 
+console.log("GENDER w, h, r:", w, h, r)
 // declare an arc generator function
 var arc = d3.svg.arc().outerRadius(r);
 
+
+console.log("GENDER arc: ", arc);
 // select paths, use arc generator to draw
 var arcs = vis.selectAll("g.slice").data(pie).enter().append("svg:g").attr("class", "slice");
 arcs.append("svg:path")
@@ -91,7 +98,7 @@ arcs.append("svg:text")
         return gender_list[i].councilperson_id_id__gender + ": " + gender_list[i].councilperson_id_id__count;});
 
 arcs.append("svg:text")
-.attr("transform", "translate(-200, -170)")
+.attr("transform", "translate(-20, -160)")
 .style("font-weight", "bold")
 .text("GENDER");
 
@@ -101,11 +108,15 @@ arcs.append("svg:text")
 ///////////////////////
 
 //Give the chart a div to go into, root svg element, a data source, attr of width, attr height, g element (groups svg shapes to work with them as if a single element), translate is a transform function that moves object x, y units
-var vis = d3.select('#party-chart').append("svg:svg").data([party_list]).attr("width", w).attr("height", h).append("svg:g").attr("transform", "translate(" + r + "," + r + ") rotate(1.6)");
+var vis = d3.select('#party-chart').append("svg:svg").data([party_list]).attr("width", w).attr("height", h).append("svg:g").attr("transform", "translate(" + r + "," + (r+30) + ") rotate(1.6)");
 var pie = d3.layout.pie().value(function(d){return d.councilperson_id_id__count;});
+
+console.log("PARTY w, h, r:", w, h, r);
 
 // declare an arc generator function
 var arc = d3.svg.arc().outerRadius(r);
+
+console.log("PARTY arc: ", arc);
 
 // select paths, use arc generator to draw
 var arcs = vis.selectAll("g.slice").data(pie).enter().append("svg:g").attr("class", "slice");
@@ -140,6 +151,6 @@ arcs.append("svg:text")
         return party_list[i].party + ": " + party_list[i].councilperson_id_id__count;});
 
 arcs.append("svg:text")
-.attr("transform", "translate(-200, -170)")
+.attr("transform", "translate(-20, -160)")
 .style("font-weight", "bold")
 .text("PARTY");
