69f241dd03da2dd7c31fdbb63f9ccd91a607b4ad
maneesha <ahseenam@gmail.com>
2015-02-04 15:11:30 -0500

    remove simple search form

--- council/urls.py
+++ council/urls.py
@@ -8,8 +8,8 @@ urlpatterns = patterns('',
     #Get the view called "index"
     #Call it when the url ends nothing (regex = ^$)
     url(r'^$', views.index, name='index'),
-    url(r'^simple_search_form$', old_views.simple_search_form, name="simple_search_form"),
-    url(r'^simple_search_results$', old_views.simple_search_results, name="simple_search_results"),
+    # url(r'^simple_search_form$', old_views.simple_search_form, name="simple_search_form"),
+    # url(r'^simple_search_results$', old_views.simple_search_results, name="simple_search_results"),
     url(r'^all_councilmembers$', views.all_councilmembers, name='all_councilmembers'),
     url(r'^unique_councilmembers$', views.unique_councilmembers, name='unique_councilmembers'),
     url(r'^demographic_breakdown$', views.demographic_breakdown, name="demographic_breakdown"),
